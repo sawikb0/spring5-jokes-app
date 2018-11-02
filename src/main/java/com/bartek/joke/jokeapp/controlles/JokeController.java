48290bbab1e85e9d865afedaf585db207a28dc5a
bartek <sawicki.bartek@hotmail.com>
2018-11-02 13:37:40 +0100

    add Joke controller

---
+++ src/main/java/com/bartek/joke/jokeapp/controlles/JokeController.java
@@ -0,0 +1,27 @@
+package com.bartek.joke.jokeapp.controlles;
+
+import com.bartek.joke.jokeapp.services.JokeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class JokeController {
+
+    private JokeService jokeService;
+
+    @Autowired
+    public JokeController(JokeService jokeService) {
+        this.jokeService = jokeService;
+    }
+
+    @RequestMapping({"/", ""})
+    public String showJoke(Model model) {
+
+        model.addAttribute("joke", jokeService.getJoke());
+
+        return "chucknorris";
+
+    }
+}
